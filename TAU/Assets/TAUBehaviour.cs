e4a467fa2e9694e8004da03cbc30df97517b294b
iceklue <iceklue@gmail.com>
2017-11-13 23:28:01 +0100

    started project

---
+++ TAU/Assets/TAUBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TAUBehaviour : MonoBehaviour {
+
+	public virtual IEnumerator ThreadedUpdate()
+	{
+		yield return null;
+	}
+
+	public virtual IEnumerator ThreadedFixedUpdate()
+	{
+		yield return null;
+	}
+
+	private void OnEnable()
+	{
+		//Enable Updates
+	}
+
+	private void OnDisable()
+	{
+		//Disable Updates
+	}
+
